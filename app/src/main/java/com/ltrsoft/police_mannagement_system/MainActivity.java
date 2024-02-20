7abae33bff6c26edcdce7a62a90bc5aca8f9a093
abu <karpudeabuhurera@gmail.com>
2024-02-20 14:40:39 +0530

    Initial commit

---
+++ app/src/main/java/com/ltrsoft/police_mannagement_system/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ltrsoft.police_mannagement_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
