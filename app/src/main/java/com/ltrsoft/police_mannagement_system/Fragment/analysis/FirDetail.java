6e997ad1b8f740331cb6e2534d7406eb20705774
omkarkshirsaga <okshirsagar700@gmail.com>
2024-03-16 14:09:45 +0530

    commited by ok

---
+++ app/src/main/java/com/ltrsoft/police_mannagement_system/Fragment/analysis/FirDetail.java
@@ -0,0 +1,28 @@
+package com.ltrsoft.police_mannagement_system.Fragment.analysis;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ltrsoft.police_mannagement_system.R;
+
+public class FirDetail extends Fragment {
+
+
+    public FirDetail() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fir_detail, container, false);
+        return view;
+    }
+}
