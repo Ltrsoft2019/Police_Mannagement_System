3aa437ef5a30ff4207e3c741782376ad63026c05
DREAMSTARTER <ganeshcollege10@gmail.com>
2024-03-18 22:11:04 +0530

    commit by ganesh

--- app/src/main/java/com/ltrsoft/police_mannagement_system/Uigraph/Fourbargraph.java
+++ app/src/main/java/com/ltrsoft/police_mannagement_system/Uigraph/Fourbargraph.java
@@ -46,6 +46,67 @@ public class Fourbargraph {
             barChart.animate();
             barChart.groupBars(0, groupSpace, barSpace);
             barChart.invalidate();
-        }
+//        barChart.getDescription().setEnabled(false);
+//
+//        // below line is to get x axis
+//        // of our bar chart.
+//        XAxis xAxis = barChart.getXAxis();
+//
+//        // below line is to set value formatter to our x-axis and
+//        // we are adding our days to our x axis.
+//        xAxis.setValueFormatter(new IndexAxisValueFormatter(days));
+//
+//        // below line is to set center axis
+//        // labels to our bar chart.
+//        xAxis.setCenterAxisLabels(true);
+//
+//        // below line is to set position
+//        // to our x-axis to bottom.
+//        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+//
+//        // below line is to set granularity
+//        // to our x axis labels.
+//        xAxis.setGranularity(1);
+//
+//        // below line is to enable
+//        // granularity to our x axis.
+//        xAxis.setGranularityEnabled(true);
+//
+//        // below line is to make our
+//        // bar chart as draggable.
+//        barChart.setDragEnabled(true);
+//
+//        // below line is to make visible
+//        // range for our bar chart.
+//        barChart.setVisibleXRangeMaximum(3);
+//
+//        // below line is to add bar
+//        // space to our chart.
+//        float barSpace = 0.1f;
+//
+//        // below line is use to add group
+//        // spacing to our bar chart.
+//        float groupSpace = 0.5f;
+//
+//        // we are setting width of
+//        // bar in below line.
+//        data.setBarWidth(0.15f);
+//
+//        // below line is to set minimum
+//        // axis to our chart.
+//        barChart.getXAxis().setAxisMinimum(0);
+//
+//        // below line is to
+//        // animate our chart.
+//        barChart.animate();
+//
+//        // below line is to group bars
+//        // and add spacing to it.
+//        barChart.groupBars(0, groupSpace, barSpace);
+//
+//        // below line is to invalidate
+//        // our bar chart.
+//        barChart.invalidate();
+    }
 
 }
