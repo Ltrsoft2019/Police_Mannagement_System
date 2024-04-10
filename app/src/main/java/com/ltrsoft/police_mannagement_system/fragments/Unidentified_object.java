9eb4514d3878d8157f84be9173796360bbe314bc
DREAMSTARTER <ganeshcollege10@gmail.com>
2024-04-10 15:23:14 +0530

    commit by ganesh

---
+++ app/src/main/java/com/ltrsoft/police_mannagement_system/fragments/Unidentified_object.java
@@ -0,0 +1,22 @@
+package com.ltrsoft.police_mannagement_system.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ltrsoft.police_mannagement_system.R;
+public class Unidentified_object extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+         View view= inflater.inflate(R.layout.unidentified_object, container, false);
+
+
+    return view;
+    }
+}
