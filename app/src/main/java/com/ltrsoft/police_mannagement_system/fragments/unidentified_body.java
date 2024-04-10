f0718ab6df79305b3c1f2ae026dfb661023f76e0
DREAMSTARTER <ganeshcollege10@gmail.com>
2024-04-10 15:26:47 +0530

    commit by ganesh

--- app/src/main/java/com/ltrsoft/police_mannagement_system/fragments/unidentified_body.java
+++ app/src/main/java/com/ltrsoft/police_mannagement_system/fragments/unidentified_body.java
@@ -9,12 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.ltrsoft.police_mannagement_system.R;
-public class Unidentified_object extends Fragment {
+public class unidentified_body extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-         View view= inflater.inflate(R.layout.unidentified_object, container, false);
+         View view= inflater.inflate(R.layout.unidentified_body, container, false);
 
 
     return view;
