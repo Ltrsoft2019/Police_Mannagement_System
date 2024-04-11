b67645b99e681d63fa488539fbd7bd9a5922a72a
DREAMSTARTER <ganeshcollege10@gmail.com>
2024-04-11 17:22:37 +0530

    commit by ganesh

--- app/src/main/java/com/ltrsoft/police_mannagement_system/Model/Apply_for_leave_model.java
+++ app/src/main/java/com/ltrsoft/police_mannagement_system/Model/Apply_for_leave_model.java
@@ -1,14 +1,6 @@
 package com.ltrsoft.police_mannagement_system.Model;
 
 public class Apply_for_leave_model {
-    String
-      leave_id  ,
-  date ,
-            type_of_leave,
-  duration ,
-  reason ,
-            remark ,
-            attachment_photo ,
-  police_id ,
-  station_id ;
+    String leave_id  , date , type_of_leave, duration , reason , remark , attachment_photo , police_id , station_id ;
+
 }
